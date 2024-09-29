e42165a3e57e6cdb501d90bc0c27182ba6b3c632
Bharathkumartirumani <bharathkumartirumani1@gmail.com>
2024-09-29 10:31:34 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,84 +1,100 @@
-import HeaderComponent from "./FunctionComponents/ImportExportEg";
-import PropsInFunction from "./FunctionComponents/UsingProps";
-import SampleClassComponent from "./ClassComponents/SampleClassComponent";
-import example from "./ClassComponents/example";
-import  Counter from "./ClassComponents/Counter";
-import SimpleClass from "./ClassComponents/SimpleClass";
-import SimpleState from "./ClassComponents/SimpleState";
-import RootComponent from "./ClassComponents/RootComponent";
-import LifeCycleMethods from "./ClassComponents/LifeCycleMethods";
-// import TodoComponent from "./ClassComponents/Todo/TodoComponent";
-import TodoComponent from "./FunctionComponents/TodoApplication/TodoComponent.js";
-import RootComponent1 from "./ClassComponents/SignInPage/RootComponent/RootComponent1";
-import LifeCycleMethods1 from "./ClassComponents/LifeCycleMethods1";
-import ComponentA from "./ClassComponents/Timer/ComponentA";
-import ComponentB from "./ClassComponents/Timer/ComponentB";
-import FormComponent from "./ClassComponents/FormComponent";
-import data from "./ClassComponents/Todo1/data";
-import TodoComponent1 from "./ClassComponents/Todo1/TodoComponent1";
-import FilterClothingProduct from "./ClassComponents/WOHOC/FilterClothingProduct";
-import clothing from "./ClassComponents/WOHOC/clothing";
-import PureComponentEg from "./ClassComponents/PureComponentEg";
-import FilterElectronicsProduct from "./ClassComponents/WOHOC/FilterElectronicsProduct";
-import productDetails from "./ClassComponents/WOHOC/electronics";
-import FilterClothingProduct1 from "./ClassComponents/WHOC/FilterClothingProduct1";
-import FilterElectronicProduct1 from "./ClassComponents/WHOC/FilterElectronicProduct1";
-import StylingComponent from "./ClassComponents/StylingComponent";
-import NormalComponent from "./ClassComponents/WHOCStyling/NormalComponent";
-import SimpleAPICall from "./ClassComponents/APICalls/SimpleAPICall";
-import LCMAPICall from "./ClassComponents/APICalls/LCMAPICalls";
-import PlainComponent from "./ClassComponents/WHOCAPICalls/PlainComponent.js";
-// import ContactManager from "./ClassComponents/ContactManager/ContactManager.js";
-import ContactManager1 from "./ClassComponents/ContactManager/ContactManager1.js";
-import FormElements from "./ClassComponents/FormExamples/FormElements.js";
-import LoginForm from "./ClassComponents/FormExamples/LoginForm.js";
-import LeaderBoard from "./ClassComponents/ScoreBoard/LeaderBoard.js";
-import Rank from "./ClassComponents/ScoreBoard/Rank.js";
-import Age from "./ClassComponents/ScoreBoard/Age.js";
-import Points from "./ClassComponents/ScoreBoard/Points.js";
-import Name from "./ClassComponents/ScoreBoard/Name.js";
-import NavBarComponent from "./ClassComponents/ScoreBoard/NavBarComponent.js";
-import LoginPage from "./ClassComponents/NestedRoutes/LoginPage.js";
-import UserPage from "./ClassComponents/NestedRoutes/UserPage.js";
-import PageNotFound from "./ClassComponents/NestedRoutes/PageNotFound.js";
-//import LoginPageFunction from "./ClassComponents/RouterHooks/LoginPage Function.js";
-import DashBoardHooks from "./ClassComponents/RouterHooks/DashBoardHooks.js";
-import PageNotFound1 from "./ClassComponents/RouterHooks/PageNotFound1.js";
-import AdminDashboard from "./ClassComponents/TicketRaisingApplication/AdminDashBoard.js";
-//import ParentComponent from "./FunctionComponents/ParentComponent.js";
-import ToggleComponent from "./FunctionComponents/ToggleComponent.js";
-import UseCaseOne from "./FunctionComponents/LCMWithHooks/UseCaseOne.js";
-import DiscriptionPage from "./ClassComponents/TicketRaisingApplication/TicketsPageClass.js";
-import TicketsPage from "./ClassComponents/TicketRaisingApplication/TicketsPageClass.js";
-import UseCaseTwo from "./FunctionComponents/LCMWithHooks/UseCaseTwo.js";
-import SignInPage from "./ClassComponents/TicketRaisingApplication/SignInPage.js";
-import AdminDashBoardInOrder from "./ClassComponents/TicketRaisingApplication/AdminDashBoardInOrder.js";
-import UseCaseThree from "./FunctionComponents/LCMWithHooks/UseCaseThree.js";
-import ContactManager from "./FunctionComponents/ContactManagerApplication/ContactManager.js";
-import HookConsumer from "./FunctionComponents/ConsumerComponent.js/HookConsumer.js";
-import PageRoot from "./FunctionComponents/PageRoot.js";
-import JsonComponent from "./FunctionComponents/ConsumerComponent.js/JsonComponent.js";
-import ArrayConsumer from "./FunctionComponents/ConsumerComponent.js/ArrayConsumer.js";
-import AlgoliaComponent from "./FunctionComponents/ConsumerComponent.js/AlgoliaComponent.js";
-import ParentComponent from "./FunctionComponents/Optimization-memo/ParentComponent.js";
-import CounterApp from "./FunctionComponents/CounterApp.js";
-import ContextProvider from "./FunctionComponents/SimpleContext/ContextProvider.js";
-import AuthProvider from "./FunctionComponents/AuthContext/AuthProvider.js";
-import ContactProvider from "./FunctionComponents/ContactContext/ContactProvider.js";
-import {BrowserRouter,Link,NavLink,Route,Routes} from 'react-router-dom';
-import AdminLoginPageFunction from "./ClassComponents/TicketRaisingApplication/AdminLoginPageFunction.js";
+// import HeaderComponent from "./FunctionComponents/ImportExportEg";
+// import PropsInFunction from "./FunctionComponents/UsingProps";
+// import SampleClassComponent from "./ClassComponents/SampleClassComponent";
+// import example from "./ClassComponents/example";
+// import  Counter from "./ClassComponents/Counter";
+// import SimpleClass from "./ClassComponents/SimpleClass";
+// import SimpleState from "./ClassComponents/SimpleState";
+// import RootComponent from "./ClassComponents/RootComponent";
+// import LifeCycleMethods from "./ClassComponents/LifeCycleMethods";
+// // import TodoComponent from "./ClassComponents/Todo/TodoComponent";
+// import TodoComponent from "./FunctionComponents/TodoApplication/TodoComponent.js";
+// import RootComponent1 from "./ClassComponents/SignInPage/RootComponent/RootComponent1";
+// import LifeCycleMethods1 from "./ClassComponents/LifeCycleMethods1";
+// import ComponentA from "./ClassComponents/Timer/ComponentA";
+// import ComponentB from "./ClassComponents/Timer/ComponentB";
+// import FormComponent from "./ClassComponents/FormComponent";
+// import data from "./ClassComponents/Todo1/data";
+// import TodoComponent1 from "./ClassComponents/Todo1/TodoComponent1";
+// import FilterClothingProduct from "./ClassComponents/WOHOC/FilterClothingProduct";
+// import clothing from "./ClassComponents/WOHOC/clothing";
+// import PureComponentEg from "./ClassComponents/PureComponentEg";
+// import FilterElectronicsProduct from "./ClassComponents/WOHOC/FilterElectronicsProduct";
+// import productDetails from "./ClassComponents/WOHOC/electronics";
+// import FilterClothingProduct1 from "./ClassComponents/WHOC/FilterClothingProduct1";
+// import FilterElectronicProduct1 from "./ClassComponents/WHOC/FilterElectronicProduct1";
+// import StylingComponent from "./ClassComponents/StylingComponent";
+// import NormalComponent from "./ClassComponents/WHOCStyling/NormalComponent";
+// import SimpleAPICall from "./ClassComponents/APICalls/SimpleAPICall";
+// import LCMAPICall from "./ClassComponents/APICalls/LCMAPICalls";
+// import PlainComponent from "./ClassComponents/WHOCAPICalls/PlainComponent.js";
+// // import ContactManager from "./ClassComponents/ContactManager/ContactManager.js";
+// import ContactManager1 from "./ClassComponents/ContactManager/ContactManager1.js";
+// import FormElements from "./ClassComponents/FormExamples/FormElements.js";
+// import LoginForm from "./ClassComponents/FormExamples/LoginForm.js";
+// import LeaderBoard from "./ClassComponents/ScoreBoard/LeaderBoard.js";
+// import Rank from "./ClassComponents/ScoreBoard/Rank.js";
+// import Age from "./ClassComponents/ScoreBoard/Age.js";
+// import Points from "./ClassComponents/ScoreBoard/Points.js";
+// import Name from "./ClassComponents/ScoreBoard/Name.js";
+// import NavBarComponent from "./ClassComponents/ScoreBoard/NavBarComponent.js";
+// import LoginPage from "./ClassComponents/NestedRoutes/LoginPage.js";
+// import UserPage from "./ClassComponents/NestedRoutes/UserPage.js";
+// import PageNotFound from "./ClassComponents/NestedRoutes/PageNotFound.js";
+// //import LoginPageFunction from "./ClassComponents/RouterHooks/LoginPage Function.js";
+// import DashBoardHooks from "./ClassComponents/RouterHooks/DashBoardHooks.js";
+// import PageNotFound1 from "./ClassComponents/RouterHooks/PageNotFound1.js";
+// import AdminDashboard from "./ClassComponents/TicketRaisingApplication/AdminDashBoard.js";
+// //import ParentComponent from "./FunctionComponents/ParentComponent.js";
+// import ToggleComponent from "./FunctionComponents/ToggleComponent.js";
+// import UseCaseOne from "./FunctionComponents/LCMWithHooks/UseCaseOne.js";
+// import DiscriptionPage from "./ClassComponents/TicketRaisingApplication/TicketsPageClass.js";
+// import TicketsPage from "./ClassComponents/TicketRaisingApplication/TicketsPageClass.js";
+// import UseCaseTwo from "./FunctionComponents/LCMWithHooks/UseCaseTwo.js";
+// import SignInPage from "./ClassComponents/TicketRaisingApplication/SignInPage.js";
+// import AdminDashBoardInOrder from "./ClassComponents/TicketRaisingApplication/AdminDashBoardInOrder.js";
+// import UseCaseThree from "./FunctionComponents/LCMWithHooks/UseCaseThree.js";
+// import ContactManager from "./FunctionComponents/ContactManagerApplication/ContactManager.js";
+// import HookConsumer from "./FunctionComponents/ConsumerComponent.js/HookConsumer.js";
+// import PageRoot from "./FunctionComponents/PageRoot.js";
+// import JsonComponent from "./FunctionComponents/ConsumerComponent.js/JsonComponent.js";
+// import ArrayConsumer from "./FunctionComponents/ConsumerComponent.js/ArrayConsumer.js";
+// import AlgoliaComponent from "./FunctionComponents/ConsumerComponent.js/AlgoliaComponent.js";
+// import ParentComponent from "./FunctionComponents/Optimization-memo/ParentComponent.js";
+// import CounterApp from "./FunctionComponents/CounterApp.js";
+// import ContextProvider from "./FunctionComponents/SimpleContext/ContextProvider.js";
+// import AuthProvider from "./FunctionComponents/AuthContext/AuthProvider.js";
+// import ContactProvider from "./FunctionComponents/ContactContext/ContactProvider.js";
+// // import {BrowserRouter,Link,NavLink,Route,Routes} from 'react-router-dom';
+// import AdminLoginPageFunction from "./ClassComponents/TicketRaisingApplication/AdminLoginPageFunction.js";
+// import { useState } from "react";
+// import TicketsFunction from "./ClassComponents/TicketRaisingApplication/TicketsFunction.js";
+// import SignInPageFunction from "./ClassComponents/TicketRaisingApplication/SignInPageFunction.js";
+// import LoginPageFunction from "./ClassComponents/TicketRaisingApplication/LoginPageFunction.js";
+// import UserSignInPageFunction from "./ClassComponents/TicketRaisingApplication/UserSignInPageFunction.js";
+// import UserLoginPageFunction from "./ClassComponents/TicketRaisingApplication/UserLoginPageFunction.js";
+// import UserTicketFunction from "./ClassComponents/TicketRaisingApplication/UserTicketFunction.js";
+// import ViewTickets from "./ClassComponents/TicketRaisingApplication/ViewTickets.js";
+// import AuthContext from "./ClassComponents/TicketRaisingApplication/AuthContext.js";
+// import AdminDashBoardFunction from "./ClassComponents/TicketRaisingApplication/AdminDashBoardFunction.js";
+// import RiseTicket from "./ClassComponents/TicketRaisingApplication/RiseTicket.js";
+// import AdminDashBoardInOrderFunction from "./ClassComponents/TicketRaisingApplication/AdminDashBoardInOrderFunction.js";
+import HomePage from "./Online-Assessment-Project/HomePage/HomePage.js";
+import { BrowserRouter, Route, Routes} from "react-router-dom"
+import AdminSignUp from "./Online-Assessment-Project/AdminSingnUp/AdminSignUp.js";
+import AdminLogIn from "./Online-Assessment-Project/AdminLogIn/AdminLogIn.js";
+import AdminDashboardFunction from "./Online-Assessment-Project/AdminDashboard/AdminDashboard.js";
+import Context from "./Online-Assessment-Project/Context/Context.js";
 import { useState } from "react";
-import TicketsFunction from "./ClassComponents/TicketRaisingApplication/TicketsFunction.js";
-import SignInPageFunction from "./ClassComponents/TicketRaisingApplication/SignInPageFunction.js";
-import LoginPageFunction from "./ClassComponents/TicketRaisingApplication/LoginPageFunction.js";
-import UserSignInPageFunction from "./ClassComponents/TicketRaisingApplication/UserSignInPageFunction.js";
-import UserLoginPageFunction from "./ClassComponents/TicketRaisingApplication/UserLoginPageFunction.js";
-import UserTicketFunction from "./ClassComponents/TicketRaisingApplication/UserTicketFunction.js";
-import ViewTickets from "./ClassComponents/TicketRaisingApplication/ViewTickets.js";
-import AuthContext from "./ClassComponents/TicketRaisingApplication/AuthContext.js";
-import AdminDashBoardFunction from "./ClassComponents/TicketRaisingApplication/AdminDashBoardFunction.js";
-import RiseTicket from "./ClassComponents/TicketRaisingApplication/RiseTicket.js";
-import AdminDashBoardInOrderFunction from "./ClassComponents/TicketRaisingApplication/AdminDashBoardInOrderFunction.js";
+import CreateQuiz1 from "./Online-Assessment-Project/AdminDashboard/CreateQuiz1.js";
+import ViewResults from "./Online-Assessment-Project/AdminDashboard/ViewResults.js";
+import UserSignUp from "./Online-Assessment-Project/UserSignUp/UserSignUp.js";
+import UserLogIn from "./Online-Assessment-Project/UserLogIn/UserLogIn.js";
+import UserDashboard from "./Online-Assessment-Project/UserDashboard/UserDashboard.js";
+import CssQuiz from "./Online-Assessment-Project/UserDashboard/CssQuiz.js";
+import HtmlQuiz from "./Online-Assessment-Project/UserDashboard/HtmlQuiz.js";
+import JsQuiz from "./Online-Assessment-Project/UserDashboard/JsQuiz.js";
+import ReactQuiz from "./Online-Assessment-Project/UserDashboard/ReactQuiz.js";
 // import clothing from "./ClassComponents/WOHOC/clothing";
 // const data=[
 //   {
@@ -107,7 +123,12 @@ import AdminDashBoardInOrderFunction from "./ClassComponents/TicketRaisingApplic
 //   }
 // ]
 function App(){
-  const [globalUsername,setUsername]=useState("")
+  // const [globalUsername,setUsername]=useState("")
+  const [globalUser, setGlobalUser] = useState({
+    Username : '',
+    Email : '',
+    Dropdown : ''
+  })
   return(
     <>
     {/* <h1>Home Page</h1> */}
@@ -216,9 +237,9 @@ function App(){
       </Routes>
       </BrowserRouter>
        */}
-       <BrowserRouter>
+       {/* <BrowserRouter>
        <AuthContext.Provider value={{globalUsername:globalUsername,setUsername:setUsername}}>
-       {/* <Link to="/"></Link>
+       <Link to="/"></Link>
        <Link to="/adminLogin"></Link>
        <Link to="/adminSignInPage"></Link>
        <Link to="/userLogin"></Link>
@@ -227,7 +248,7 @@ function App(){
        <Link to="adminDashboardInOrder"></Link>
        <Link to="/tickets"></Link>
        <Link to="/userTicket"></Link>
-       <Link to="/viewTicket"></Link> */}
+       <Link to="/viewTicket"></Link>
        <Routes>
         <Route path="/" element={<LoginPageFunction></LoginPageFunction>}></Route>
         <Route path="/adminLogin" element={<AdminLoginPageFunction></AdminLoginPageFunction>}></Route>
@@ -242,7 +263,7 @@ function App(){
         <Route path="/riseTicket" element={<RiseTicket></RiseTicket>}></Route>
         </Routes>
         </AuthContext.Provider>
-        </BrowserRouter>
+        </BrowserRouter> */}
       {/* <ParentComponent></ParentComponent> */}
       {/* <ToggleComponent></ToggleComponent> */}
       {/* <UseCaseOne></UseCaseOne> */}
@@ -262,7 +283,29 @@ function App(){
       {/* <AuthProvider></AuthProvider> */}
       {/* <ContextProvider></ContextProvider> */}
       {/* <ContactProvider></ContactProvider> */}
-
+      <BrowserRouter>
+      <Context.Provider value={{globalUser:globalUser,setGlobalUser:setGlobalUser}}>
+        <Routes>
+        {/* <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route component={NotFound} /> */}
+           <Route path="/" element={<HomePage></HomePage>}> </Route>
+           <Route path="/adminSignUp" element={<AdminSignUp></AdminSignUp>}></Route>
+           <Route path="userSignUp" element={<UserSignUp></UserSignUp>}></Route>
+           <Route path="/adminLogin" element={<AdminLogIn></AdminLogIn>}></Route>
+           <Route path="/userLogin" element={<UserLogIn></UserLogIn>}></Route>
+           <Route path="/adminDashboard" element={<AdminDashboardFunction></AdminDashboardFunction>}></Route>
+           <Route path="/userDashboard" element={<UserDashboard></UserDashboard>}></Route>
+           <Route path="/createQuiz" element={<CreateQuiz1></CreateQuiz1>}></Route>
+           <Route path="/viewResults" element={<ViewResults></ViewResults>}></Route>
+           <Route path="/cssQuiz" element={<CssQuiz></CssQuiz>}></Route>
+           <Route path="/htmlQuiz" element={<HtmlQuiz></HtmlQuiz>}></Route>
+           <Route path="/jsQuiz" element={<JsQuiz></JsQuiz>}></Route>
+           <Route path="/reactQuiz" element={<ReactQuiz></ReactQuiz>}></Route>
+           </Routes>
+           </Context.Provider>
+ </BrowserRouter>
     </>
   )
 }
